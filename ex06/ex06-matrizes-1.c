b2ca9e9e89c620d6ec805d23ea59b6db3f52ec51
Marcio Bueno <marciobueno@gmail.com>
2026-09-14 08:42:21 -0300

    matriz (refatoração de código para utilizar funções)

--- ex06/ex06-matrizes-1.c
+++ ex06/ex06-matrizes-1.c
@@ -3,20 +3,47 @@
 #define QTD_LINHAS 4
 #define QTD_COLUNAS 3
 
+void preencherMatriz(int m[][QTD_COLUNAS], int lins, int cols);
+void imprimirMatriz(int m[][QTD_COLUNAS], int lins, int cols);
+void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols);
+
 int main() {
-    int m[QTD_LINHAS][QTD_COLUNAS];
-    for (int i = 0; i < QTD_LINHAS; i += 1) {
-        for (int j = 0; j < QTD_COLUNAS; j += 1) {
+    int matriz[QTD_LINHAS][QTD_COLUNAS];
+    preencherMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
+    printf("\nImpressão Matriz M\n");
+    imprimirMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
+    imprimirMaiorValorMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
+    return 0;
+}
+
+void preencherMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
+    for (int i = 0; i < lins; i += 1) {
+        for (int j = 0; j < cols; j += 1) {
             printf("Digite o valor de m[%d][%d]:\n", i + 1, j + 1);
             scanf("%d", &m[i][j]);
         }
     }
-    printf("\nImpressão Matriz M\n");
-    for (int i = 0; i < QTD_LINHAS; i += 1) {
-        for (int j = 0; j < QTD_COLUNAS; j += 1) {
+}
+
+void imprimirMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
+    for (int i = 0; i < lins; i += 1) {
+        for (int j = 0; j < cols; j += 1) {
             printf("%2d ", m[i][j]);
         }
         printf("\n");
     }
-    return 0;
+}
+
+void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
+    int iMaior = 0, jMaior = 0;
+    for (int i = 0; i < lins; i += 1) {
+        for (int j = 0; j < cols; j += 1) {
+            if (m[i][j] > m[iMaior][jMaior]) {
+                iMaior = i;
+                jMaior = j;
+            }
+        }
+    }
+    printf("O maior valor é %d e está na posição (%d, %d)\n",
+        m[iMaior][jMaior], iMaior + 1, jMaior + 1);
 }
