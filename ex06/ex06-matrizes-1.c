d25f763e1f43a3c54b5c7e13790005cf90b46b01
Marcio Bueno <marciobueno@gmail.com>
2026-09-17 07:48:52 -0300

    maior valor matriz (passagem por referência)

--- ex06/ex06-matrizes-1.c
+++ ex06/ex06-matrizes-1.c
@@ -5,14 +5,18 @@
 
 void preencherMatriz(int m[][QTD_COLUNAS], int lins, int cols);
 void imprimirMatriz(int m[][QTD_COLUNAS], int lins, int cols);
-void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols);
+void maiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols, int *posI, int *posJ);
+void buscarMatriz(int m[][QTD_COLUNAS], int lins, int cols, int x);
 
 int main() {
+    int iMaior, jMaior;
     int matriz[QTD_LINHAS][QTD_COLUNAS];
     preencherMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
     printf("\nImpressão Matriz M\n");
     imprimirMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
-    imprimirMaiorValorMatriz(matriz, QTD_LINHAS, QTD_COLUNAS);
+    maiorValorMatriz(matriz, QTD_LINHAS, QTD_COLUNAS, &iMaior, &jMaior);
+    printf("O maior valor é %d e está na posição (%d, %d)\n",
+        matriz[iMaior][jMaior], iMaior + 1, jMaior + 1);
     return 0;
 }
 
@@ -34,7 +38,9 @@ void imprimirMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
     }
 }
 
-void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
+void maiorValorMatriz(
+    int m[][QTD_COLUNAS], int lins, int cols, int *posI, int *posJ
+) {
     int iMaior = 0, jMaior = 0;
     for (int i = 0; i < lins; i += 1) {
         for (int j = 0; j < cols; j += 1) {
@@ -44,6 +50,10 @@ void imprimirMaiorValorMatriz(int m[][QTD_COLUNAS], int lins, int cols) {
             }
         }
     }
-    printf("O maior valor é %d e está na posição (%d, %d)\n",
-        m[iMaior][jMaior], iMaior + 1, jMaior + 1);
+    *posI = iMaior;
+    *posJ = jMaior;
+}
+
+void buscarMatriz(int m[][QTD_COLUNAS], int lins, int cols, int x) {
+
 }
